ce1f7cc6385061f19589ffb3cf5afab35e4f1b3b
David María Arribas <david.maria@urjc.es>
2025-07-14 18:28:45 +0200

    New: initial commit

---
+++ src/generator.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+from scipy.signal import spectrogram
+
+from src.loader import load_sensor_data, sensor_cols, load_processed_data
+
+
+def get_clean_blocks(data: pd.DataFrame, window_time: int = 30):
+    is_normal = data['Label'] == 'Normal'
+    changes = is_normal.astype(int).diff().fillna(0)
+
+    data['rel_timestamp'] = data['timestamp'] - data['timestamp'].iloc[0]
+    starts = data.loc[changes == 1, 'rel_timestamp'].index
+    ends = data.loc[changes == -1, 'rel_timestamp'].index
+
+    if is_normal.iloc[0]:
+        starts = np.r_[data.index[0], starts]
+    if is_normal.iloc[-1]:
+        ends = np.r_[ends, data.index[-1]]
+
+    t_max = data['rel_timestamp'].max()
+    half_window_time = window_time / 2.0
+    blocks = []
+    for start_idx, end_idx in zip(starts, ends):
+        block_start = data.loc[start_idx, 'rel_timestamp']
+        block_end = data.loc[end_idx, 'rel_timestamp']
+        ex_start = block_start + half_window_time
+        ex_end = block_end - half_window_time
+        selected_data = data[(data['rel_timestamp'] >= ex_start) & (data['rel_timestamp'] <= ex_end)]
+
+        if selected_data.empty:
+            continue
+
+        blocks.append({
+            'data': selected_data
+        })
+
+    return blocks
+
+
+def get_anomaly_intervals(data: pd.DataFrame, window_time: int = 30):
+    is_anom = data['Label'] != 'Normal'
+
+    changes = is_anom.astype(int).diff().fillna(0)
+
+    data['rel_timestamp'] = data['timestamp'] - data['timestamp'].iloc[0]
+    starts = data.loc[changes == 1, 'rel_timestamp'].index
+    ends = data.loc[changes == -1, 'rel_timestamp'].index
+
+    if is_anom.iloc[0]:
+        starts = np.r_[data.index[0], starts]
+    if is_anom.iloc[-1]:
+        ends = np.r_[ends, data.index[-1]]
+
+    t_max = data['rel_timestamp'].max()
+    half_window_time = window_time / 2.0
+    segments = []
+
+    for start_idx, end_idx in zip(starts, ends):
+        label = data.loc[start_idx, 'Label']
+        anomaly_start = data.loc[start_idx, 'rel_timestamp']
+        anomaly_end = data.loc[end_idx, 'rel_timestamp']
+        center = (anomaly_start + anomaly_end) / 2.0
+        ex_start = max(0, center - half_window_time)
+        ex_end = min(t_max, center + half_window_time)
+        seg_df = data[(data['rel_timestamp'] >= ex_start) &
+                      (data['rel_timestamp'] <= ex_end)].copy()
+
+        segments.append({
+            'label': label,
+            'start': start_idx,
+            'end': end_idx,
+            'data': seg_df
+        })
+
+    return segments
+
+
+def resample(data: pd.DataFrame, sample_rate: int):
+    return data.resample(f"{sample_rate}ms").mean().interpolate(
+        method="linear", limit_direction="both"
+    )
+
+
+def generate_spectrogram(data: pd.DataFrame, magnitude: str):
+    fs = np.round(float(1.0 / data['delta_time'].iloc[1]))
+
+    f, t_spec, Sxx = spectrogram(data[magnitude].values,
+                                 fs=fs,
+                                 window='hann',
+                                 nperseg=50,
+                                 noverlap=45,
+                                 nfft=512
+                                 )
+    return f, t_spec, Sxx
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    magnitude = sensor_cols[5]
+    data = load_processed_data("data/merged_data.csv")
+    f, t_spec, Sxx = generate_spectrogram(data, magnitude=magnitude)
+
+    Sxx_dB = 10 * np.log10(Sxx + 1e-20)
+
+    plt.figure(figsize=(6, 8))
+    # transpose Sxx so rows=times, cols=freqs
+    plt.pcolormesh(f, t_spec, Sxx_dB.T, shading='gouraud')
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Time [sec]')
+    plt.title(f'Spectrogram of {magnitude} over time')
+    plt.colorbar(label='Power spectral density')
+    plt.tight_layout()
+    plt.show()
---
+++ src/signal_tools.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def a_weight(f):
+    f2 = f ** 2
+    return 1.2588966 * 148840000 * f2 * f2 / ((f2 + 424.36) * np.sqrt((f2 + 11599.29) *
+                                                                      (f2 + 544496.41)) * (f2 + 148840000))
+
+def transform_to_db(data: np.ndarray, reference: int = 10):
+    return reference * np.log10(data + 1e-20)
+
+
+
+def extract_spectrogram(Sxx: np.ndarray, f: np.ndarray, t_spec: np.ndarray, start: float, length:float):
+    end = start + length
+
+    # find the first bin ≥ start, and the first bin > end
+    i0 = np.searchsorted(t_spec, start, side='left')
+    i1 = np.searchsorted(t_spec, end, side='right')
+
+    # clamp to valid range
+    i0 = max(0, i0)
+    i1 = min(len(t_spec), i1)
+    if i0 >= i1:
+        raise ValueError(f"No spectrogram bins in [{start}, {end}]")
+
+    Sxx_seg = Sxx[:, i0:i1]
+    t_seg = t_spec[i0:i1]
+
+    return Sxx_seg, f, t_seg, i0, i1
